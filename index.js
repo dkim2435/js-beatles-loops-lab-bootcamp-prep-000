b2c11cadd127f6a52c41207d51484b29bfc4d453
Evan Kim <dkim2435@gmail.com>
2019-04-12 09:42:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // add solution here
+function theBeatlesPlay() {
+  var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+  var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+ var empty = new Array();
+ let MLength = musicians.length;
+ let ILength = instruments.length;
+ 
+ for (let i = 0; i<MLength;i++) {
+   for (let j = 0; j < ILength; j++) {
+     console.log(empty[musicians[i] + " plays " +instruments[j]]);
+   }
+ }
+  
+  
+  
+  
+  
+  
+}
